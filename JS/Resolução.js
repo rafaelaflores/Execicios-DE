5f319d4fae6437ed01852bb9cde65c3cc47a9b33
Rafaela Jardim <80408715+rafaelaflores@users.noreply.github.com>
2023-02-03 16:36:10 -0300

    Update Resolução.js

--- JS/Resolução.js
+++ JS/Resolução.js
@@ -38,35 +38,3 @@ function formataTelefone(telefone) {
 }
 
 console.log(formataTelefone(numero));
-
-
-async function a() {
-    b();
-    await c();
-    await d();
-    alertUser("a");   
-}
-
-a();
-
-function b() {
-    return;
-    alertUser('b');   
-}
-
-function c() {
-    return new Promise((resolve) => {
-        resolve();
-        alertUser('c');   
-    });
-}
-
-function d() {
-    return new Promise((resolve) => {
-        alertUser('d');   
-    });
-}
-
-function alertUser(message) {
-    console.log('A função é: ' + message)
-}
